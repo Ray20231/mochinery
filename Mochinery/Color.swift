22c5606cecad7dfc5bd01b326c6ce0f93e2f8f25
reichel <reiya.2020@gmail.com>
2024-02-07 18:39:54 +0900

    Initial commit

---
+++ Mochinery/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  Mochinery
+//
+//  Created by 宮水三葉 on 2024/02/07.
+//
+
+import Foundation
---
+++ Mochinery/contents/LogStartView.swift
@@ -0,0 +1,8 @@
+//
+//  LogStartView.swift
+//  Mochinery
+//
+//  Created by 宮水三葉 on 2024/02/07.
+//
+
+import Foundation
